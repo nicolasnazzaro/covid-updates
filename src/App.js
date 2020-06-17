c56a04ec611c319f0c46e9f071d1336f4f2c9cbb
nicolasnazzaro <nicolas.nazzaro@gmail.com>
2020-06-17 21:12:20 +0200

    Initial fetching data in app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from './components/cards/Cards';
 import Chart from './components/chart/Chart';
 import CountryPicker from './components/countryPicker/CountryPicker';
+import { fetchData } from './api/agent';
+
+
 
 const App = () => {
+  
+  const [data, setData] = useState({});
+  
+  useEffect(() => {
+    fetchData().then(response => setData({...response}));
+  },[]);
+  
   return (
     <div>
-      <Cards/>
+      <Cards data={data}/>
       <Chart/>
       <CountryPicker/>
     </div>
